15876d72c8ff7c1e7f567a6595a7e14b098042ab
Aleksei Zhuravlev <aa_zhur@mail.ru>
2018-02-28 21:48:16 +0300

    Add dumping and reading from dump

--- optimization.h
+++ optimization.h
@@ -3,6 +3,7 @@
 #include "classes.h"
 #include <ctime>
 #include <algorithm>
+#include <fstream>
 
 // Interface to be defined
 vector<char> AddBallon(const Solution& current);
@@ -12,6 +13,27 @@ struct CSolutionWithScore {
 	int Score;
 	bool operator<(const CSolutionWithScore& other) {
 		return Score > other.Score;
+	}
+	void write(ofstream& stream) {
+		stream << Solution.size();
+		stream << Solution[0].size();
+		for (int i = 0; i < Solution.size(); i++) {
+			for (int j = 0; j < Solution[0].size(); j++) {
+				stream << Solution[i][j];
+			}
+		}
+		stream << Score;
+	}
+	void read(ifstream& stream) {
+		int n, m;
+		stream >> n >> m;
+		Solution.assign(n, vector<char>(m, 0));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < Solution[0].size(); j++) {
+				stream >> Solution[i][j];
+			}
+		}
+		stream >> Score;
 	}
 };
 
@@ -40,6 +62,30 @@ void nextGeneration(Population& current, const int MaxGenerationSize, const int 
 	}
 }
 
+void dumpPopulation(Population& population, char* filename = "gen.pop")
+{
+	ofstream outfile;
+	outfile.open( filename, ios::out | ios::trunc);
+	outfile << population.size();
+	for (int i = 0; i < population.size(); i++) {
+		population[i].write(outfile);
+	}
+	outfile.close();
+}
+
+void readPopulation(Population& population, char* filename = "gen.pop")
+{
+	ifstream infile;
+	infile.open(filename, ios::in);
+	int size;
+	infile >> size;
+	population.resize(size);
+	for (int i = 0; i < size; i++) {
+		population[i].read(infile);
+	}
+	infile.close();
+}
+
 Solution impove(const Solution& solution, const int NumberOfIterations = 100, const int MaxGenerationSize = 100, const int GrowingSize = 10, const int MaxSingleChanges = 3 )
 {
 	Population population;
@@ -48,6 +94,19 @@ Solution impove(const Solution& solution, const int NumberOfIterations = 100, co
 	for (int i = 0; i < NumberOfIterations; i++) {
 		printf("Gen %d Best %d\n", i, population[0].Score);
 		nextGeneration(population, MaxGenerationSize, GrowingSize, MaxSingleChanges);
+		dumpPopulation(population);
+	}
+	return population[0].Solution;
+}
+
+Solution improveFromDump(const int NumberOfIterations = 100, const int MaxGenerationSize = 100, const int GrowingSize = 10, const int MaxSingleChanges = 3)
+{
+	Population population;
+	readPopulation(population);
+	for (int i = 0; i < NumberOfIterations; i++) {
+		printf("Gen %d Best %d\n", i, population[0].Score);
+		nextGeneration(population, MaxGenerationSize, GrowingSize, MaxSingleChanges);
+		dumpPopulation(population);
 	}
 	return population[0].Solution;
 }
