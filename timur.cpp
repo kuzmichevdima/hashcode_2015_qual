aa8744f5569912e3baef123df4f277e1c8161577
Timur <tkhismatullin@yandex-team.ru>
2018-02-28 21:11:37 +0300

    q

--- timur.cpp
+++ timur.cpp
@@ -4,21 +4,60 @@ struct State {
     int cost, prev;
 };
 
-Position move_baloon(const Position& p) {
-
+vector<pii> get_coverage(const pii& pos) {
+    vector<pii> res;
+    for (int x = pos.first - V; x <= pos.first + V; ++x) {
+        if (x < 0 || x >= grid.R) {
+            continue;
+        }
+        for (int Y = pos.second - V; Y <= pos.second + V; ++Y) {
+            int y = Y;
+            if (y < 0) {
+                y += grid.C;
+            } else if (y >= grid.C) {
+                y -= grid.C;
+            }
+            int deltay = abs(y - pos.second);
+            int col_dist = min(deltay, grid.C - deltay);
+            int tmp = sqr(x - pos.fi) + sqr(col_dist);
+            if (tmp <= V * V) {
+                res.emplace_back(x, y);
+            }
+        }
+    }
+    return res;
 }
 
-vector<vector<vector<int>>> get_cost(const Grid& grid, int V, int T, const vector<pii>& targets,
-                                     const vector<vector<char>>& balloons) {
+
+vector<vector<vector<int>>> get_cost(const vector<vector<char>>& balloons) {
+    vector<vector<vector<int>>> res(T, vector<vector<int>>(grid.R, vector<int>(grid.C)));
+    for (auto& t : targets) {
+        for (auto & pos : get_coverage(t)) {
+            ++res[0][pos.fi][pos.se];
+        }
+    }
+
+    for (int t = 1; t <= T; ++t) {
+        for (int x = 0; x < grid.R; x++) {
+            for (int y = 0; y < grid.C; y++) {
+                res[t][x][y] = res[t - 1][x][y];
+                for (auto& b : balloons) {
+                    auto pos = start_cell;
+
+                }
+            }
+        }
+    }
+
 
 }
 
 int main() {
-    read_input(grid, V, B, T, start_cell, targets);
+    read_input();
 
     vector<vector<vector<vector<State>>>> dp;
 
-    vector<vector<vector<int>>> cost = init_cost(grid, targets);
+    vector<vector<vector<int>>> cost = get_cost({});
 
 
     for (int balloon = 0; balloon < B; ++balloon) {
